444cdca84fcc8b2e3ad85bd50e91d4053050021b
Rishu Singh <132397721+Rishu18D@users.noreply.github.com>
2023-12-21 13:24:12 +0530

    todolist

---
+++ app.js
@@ -0,0 +1,18 @@
+// public/app.js
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText !== '') {
+    const li = document.createElement('li');
+    li.innerHTML = `${taskText} <button onclick="removeTask(this)">Remove</button>`;
+    taskList.appendChild(li);
+    taskInput.value = '';
+  }
+}
+
+function removeTask(button) {
+  const li = button.parentElement;
+  li.remove();
+}
